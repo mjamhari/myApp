ba8078cceb9d8f672284cc9122d9f7b6db2842e1
mjamhari <mjamhari@utm.my>
2018-11-27 09:21:52 -0800

    first commit

---
+++ src/pages/action/action.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Platform, ActionSheetController} from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+
+@Component({
+    selector: 'page-action',
+  templateUrl: 'action.html'
+})
+export class ActionPage {
+  constructor(
+      public NavCtrl: NavController,
+    public platform: Platform,
+    public actionsheetCtrl: ActionSheetController,
+    public alertCtrl: AlertController
+  ) { }
+
+
+  doAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Albums',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Share',
+          icon: !this.platform.is('ios') ? 'share' : null,
+          handler: () => {
+            console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Play',
+          icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('Play clicked');
+          }
+        },
+        {
+          text: 'Favorite',
+          icon: !this.platform.is('ios') ? 'heart-outline' : null,
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: !this.platform.is('ios') ? 'close' : null,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+}
